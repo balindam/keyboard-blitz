efd35c96959f4c0bc89dc0a39f8aa910cf827c43
balindam <balindam.c@gmail.com>
2024-03-06 21:41:19 +0530

    feat: add footer styling and absolute import

--- apps/web-ui/src/components/Footer/index.tsx
+++ apps/web-ui/src/components/Footer/index.tsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import './style.scss'
+import IconButton from '@components/common/IconButton/IconButton'
 
 type Props = {}
 
@@ -18,6 +19,24 @@ const footerActionButtons = [
   }
 ]
 
+const footerIconButtons = [
+  {
+    name: 'facebook',
+    iconPath: '/assets/icons/facebook-icon.png',
+    redirectURL: 'https://facebook.com',
+  },
+  {
+    name: 'twitter',
+    iconPath: 'assets/icons/twitter-icon.png',
+    redirectURL: 'https://twitter.com',
+  },
+  {
+    name: 'instagram',
+    iconPath: 'assets/icons/instagram-icon.png',
+    redirectURL: 'https://instagram.com',
+  }
+]
+
 const Footer = (props: Props) => {
   return (
     <div className="_footer-container">
@@ -29,7 +48,14 @@ const Footer = (props: Props) => {
         ))}
       </div>
       <div className="_footer-section-2">
-
+          {footerIconButtons.map((button, index) => (
+              <IconButton
+                key={`${button.name}-${index}`}
+                imgSRC={button.iconPath}
+                altText={button.name}
+                handleClick={() => window.open(button.redirectURL, '_blank')}
+              />
+            ))}
       </div>
       <div className="_footer-section-3">
         ©2024 Keyboard Blitz. All rights reserved.
