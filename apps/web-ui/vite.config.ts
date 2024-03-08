21f1a8461798fc4d204e48fa7cd76028133e28ac
balindam <balindam.c@gmail.com>
2024-03-08 13:44:07 +0530

    feat: styled navbar and deploy to gh-pages

--- apps/web-ui/vite.config.ts
+++ apps/web-ui/vite.config.ts
@@ -1,5 +1,6 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import svgr from "vite-plugin-svgr";
 import path from 'path'
 
 // https://vitejs.dev/config/
@@ -8,8 +9,8 @@ export default defineConfig({
     alias: {
       '@': path.resolve(__dirname, 'src'),
       '@components': path.resolve(__dirname, 'src/components'),
-      '@assets': path.resolve(__dirname, 'src/assets')
+      '@assets': path.resolve(__dirname, 'public/assets')
     }
   },
-  plugins: [react()]
+  plugins: [react(), svgr()]
 });
